679fa017b8241af1068308717c7d159a7bde2c0a
Rolo-Tomassi-037 <cdacinfowaygroup037@gmail.com>
2022-11-09 16:00:09 +0530

    User input fetch and display result structure created

--- calculator.js
+++ calculator.js
@@ -1,14 +1,30 @@
 
 function additionNum(){
+    var first_number = parseInt(document.getElementById("input_1").value);
+    var second_number = parseInt(document.getElementById("input_2").value);
+    var result;
+    document.getElementById("result").innerHTML = "Result :" + result;
 }
 
 function subtractionNum(){
+    var first_number = parseInt(document.getElementById("input_1").value);
+    var second_number = parseInt(document.getElementById("input_2").value);
+    var result;
+    document.getElementById("result").innerHTML = "Result :" + result;
 }
 
 function multiplicationNum(){
+    var first_number = parseInt(document.getElementById("input_1").value);
+    var second_number = parseInt(document.getElementById("input_2").value);
+    var result;
+    document.getElementById("result").innerHTML = "Result :" + result;
 }
 
 function divisionNum(){
+    var first_number = parseInt(document.getElementById("input_1").value);
+    var second_number = parseInt(document.getElementById("input_2").value);
+    var result;
+    document.getElementById("result").innerHTML = "Result :" + result;
 }
 
 document.getElementById("add_btn").onclick=additionNum
