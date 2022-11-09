e9894d348ae5134010faf47d18145e91964ddbb0
Rolo-Tomassi-037 <cdacinfowaygroup037@gmail.com>
2022-11-09 15:55:42 +0530

    Initial js structure created

---
+++ calculator.js
@@ -0,0 +1,17 @@
+
+function additionNum(){
+}
+
+function subtractionNum(){
+}
+
+function multiplicationNum(){
+}
+
+function divisionNum(){
+}
+
+document.getElementById("add_btn").onclick=additionNum
+document.getElementById("sub_btn").onclick=subtractionNum
+document.getElementById("mul_btn").onclick=multiplicationNum
+document.getElementById("div_btn").onclick=divisionNum
