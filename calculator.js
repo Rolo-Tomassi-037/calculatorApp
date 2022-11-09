7f110d8707c1e752b88c7bc6c3308637e642bc6f
Rolo-Tomassi-037 <cdacinfowaygroup037@gmail.com>
2022-11-09 17:22:09 +0530

    Added square and cube functionality in js file

--- calculator.js
+++ calculator.js
@@ -27,7 +27,25 @@ function divisionNum(){
     document.getElementById("result").innerHTML = "Result :" + result;
 }
 
+function squareNum(){
+    var first_number = Number(document.getElementById("input_1").value);
+    var second_number = Number(document.getElementById("input_2").value);
+    var result1 = first_number * first_number;
+    var result2 = second_number * second_number;
+    document.getElementById("result").innerHTML = "Result :" + result1 + ", " + result2;
+}
+
+function cubeNum(){
+    var first_number = Number(document.getElementById("input_1").value);
+    var second_number = Number(document.getElementById("input_2").value);
+    var result1 = first_number * first_number * first_number;
+    var result2 = second_number * second_number * second_number;
+    document.getElementById("result").innerHTML = "Result :" + result1 + ", " + result2;
+}
+
 document.getElementById("add_btn").onclick=additionNum
 document.getElementById("sub_btn").onclick=subtractionNum
 document.getElementById("mul_btn").onclick=multiplicationNum
 document.getElementById("div_btn").onclick=divisionNum
+document.getElementById("sq_btn").onclick=squareNum
+document.getElementById("cube_btn").onclick=cubeNum
